30332447a5ede6a348302b11b46918fa39da96fc
tamagoo002 <anamikarai0020806@gmail.com>
2024-10-24 09:31:34 +0545

    finalized ui

--- clone/src/App.js
+++ clone/src/App.js
@@ -5,6 +5,10 @@ import msgIcon from './assets/message.svg';
 import home from'./assets/home.svg';
 import saved from'./assets/bookmark.svg';
 import rocket from'./assets/rocket.svg';
+import sendBtn from './assets/send.svg';
+import userIcon from './assets/user-icon.png';
+import gptImgLogo from './assets/chatgptLogo.svg';
+
 
 function App() {
   return (
@@ -38,7 +42,21 @@ function App() {
 
       {/* Main Content */}
       <div className="main">
-        {/* Main content can go here */}
+       <div className="chats">
+        <div className="chat">
+          <img className='chatimg' src={userIcon} alt="" /><p className="txt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, voluptatem? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam iure impedit repudiandae, veritatis dicta veniam atque sequi explicabo officiis nulla.</p></div>
+          <div className="chat bot">
+          <img className='chatimg' src={gptImgLogo} alt="" /><p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus quasi officia rem accusamus. Cum quidem laudantium aspernatur error, sint reprehenderit, doloribus quia aliquam sunt unde quo quae molestiae similique voluptas maiores doloremque recusandae quod nesciunt modi. Atque quidem quaerat neque aliquam pariatur sequi assumenda eum ratione nam distinctio doloremque optio dolore nemo, enim cumque delectus asperiores recusandae, necessitatibus eligendi, nostrum vero explicabo molestiae aspernatur numquam? Quidem corporis quis asperiores optio, officia maxime. Error, earum neque, fugiat pariatur debitis eius suscipit commodi at ullam dignissimos quod nemo excepturi. Labore reiciendis, minus quia enim mollitia esse magnam tempora? Quas eligendi repellat exercitationem!</p></div>
+       </div>
+       
+       <div className="chatFooter">
+        <div className="inp">
+          <input type="text" placeholder='Enter Message here' /> <button className="send"><img src={sendBtn} alt="SEnd" /></button>
+          
+
+        </div>
+        <p>ChatGPT can make mistakes. Check important info.</p>
+       </div>
       </div>
     </div>
   );
